cc51ec5301f00e4386e07fa14603a00a3231102d
ektrah <klaus.hartke@gmail.com>
2016-10-16 04:06:42 +0200

    Update description

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,10 +9,10 @@ namespace Reflow
     {
         public const string Id = "Reflow.2af72dbc-b8ef-438a-bc03-922e5acf446c";
         public const string Name = "Reflow";
-        public const string Description = @"Formats the current selection to have lines no longer than 80 characters. If nothing is selected, the current line is reflowed.";
+        public const string Description = @"Formats the current selection to have lines no longer than 80 characters. If nothing is selected, the current line is formatted.";
         public const string Language = "en-US";
         public const string Version = "1.0.1";
         public const string Author = "Klaus Hartke";
-        public const string Tags = "format, reflow";
+        public const string Tags = "Editor, Text, format, reflow";
     }
 }
