48df1036f9cef89397550bd14d2a999a73ef598c
ektrah <klaus.hartke@gmail.com>
2016-10-18 21:21:44 +0200

    Rename package

--- src/ReflowPackage.cs
+++ src/ReflowPackage.cs
@@ -10,5 +10,5 @@ namespace Reflow
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.guidReflowPackageString)]
     [ProvideAutoLoad(VSConstants.VsEditorFactoryGuid.TextEditor_string)]
-    public sealed class VSPackage : Package { }
+    public sealed class ReflowPackage : Package { }
 }
