b1888d530ece9bb2eb74d5380997861e50f6f451
ektrah <ektrah@users.noreply.github.com>
2019-04-13 14:20:56 +0200

    Add support for Visual Studio 2019

--- src/ReflowPackage.cs
+++ src/ReflowPackage.cs
@@ -5,19 +5,20 @@ using Microsoft.VisualStudio.Shell;
 
 namespace Reflow
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.guidReflowPackageString)]
-    [ProvideAutoLoad(VSConstants.VsEditorFactoryGuid.TextEditor_string)]
+    [ProvideAutoLoad(VSConstants.VsEditorFactoryGuid.TextEditor_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideOptionPage(typeof(ReflowOptionPage), "Environment", "Reflow", 0, 0, true, new[] { "text", "markdown", "reflow" })]
-    public sealed class ReflowPackage : Package
+    public sealed class ReflowPackage : AsyncPackage
     {
         public static ReflowOptionPage Options { get; private set; }
 
-        protected override void Initialize()
+        protected override async System.Threading.Tasks.Task InitializeAsync(System.Threading.CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            base.Initialize();
+            await base.InitializeAsync(cancellationToken, progress);
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
             Options = (ReflowOptionPage)GetDialogPage(typeof(ReflowOptionPage));
         }
     }
